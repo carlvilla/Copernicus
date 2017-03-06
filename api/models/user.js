38831e9f2fb94fcd924f08fcdcb8fab9a1ad6f15
Carlos Villa <uo236851@uniovi.es>
2017-03-06 18:26:25 +0100

    Middleware, seraph y eliminado node_modules

--- api/models/user.js
+++ api/models/user.js
@@ -1,20 +1,22 @@
 var confDB = require('../config/db')
-var db = require('seraph')({
-    URI: confDB.db.URI,
+
+var seraph = require('seraph')({
+    server: confDB.db.server,
     user: confDB.db.user,
-    pass: confDB.db.pass});
+    pass: confDB.db.pass
+});
 
 var model = require('seraph-model');
 
-var user = model(db, 'User');
+var user = model(seraph, 'User');
+
+var emailRegex = /^(?:[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+\.)*[\w\!\#\$\%\&\'\*\+\-\/\=\?\^\`\{\|\}\~]+@(?:(?:(?:[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!\.)){0,61}[a-zA-Z0-9]?\.)+[a-zA-Z0-9](?:[a-zA-Z0-9\-](?!$)){0,61}[a-zA-Z0-9]?)|(?:\[(?:(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\.){3}(?:[01]?\d{1,2}|2[0-4]\d|25[0-5])\]))$/;
 
 user.schema = {
     username: { type: String, required: true},
     nombre: { type: String, required: true },
     apellidos: {type: String, required: true },
-    email: { type: String, match: emailRegex, required: true },
-    hash: { type: String },
-    salt: {type: String}
-
-
-}
+    email: { type: String,match: emailRegex, required: true },
+    hash: { type: String},
+    salt: { type: String}
+};
--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,49 +1,14 @@
 var express = require('express');
-var jwt = require("jwt-simple");
 var router = express.Router();
 var ctrl = require('../controllers/pagesControllers');
+var middleware = require('./middleware');
 
 router.get('/', ctrl.index);
-router.get('/about', ctrl.about);
-router.get('/index', ctrl.index);
-
-
-// Middleware para verificar si existencia el token generado al iniciar sesión
-//Todas las nuevas routas añadidas después de esta llamada necesitarán el token
-router.use(function(req, res, next) {
-
-    var token = req.cookies.token;
-
-    if (token) {
-        try {
-            var payload = jwt.decode(token, process.env.JWT_SECRET);
-            next();
-        }
-
-        catch (err) {
-            res.redirect('./index');
-            var payload = jwt.decode(token, process.env.JWT_SECRET);
-            //En el caso de que el token hubiese expirado
-            if (payload.exp <= moment().unix()) {
-                res.redirect('./index');
-                req.sub = payload.sub;
-                next();
-            }
-        }
-
-    }else {
-        res.redirect('./index');
-        return res.status(403).send({
-            success: false,
-            message: 'No existe token de sesión'
-        });
-    }
-});
-
-
-router.get('/profile', ctrl.profile); //Página en la que el usuario puede cambiar sus datos personales
-router.get('/searchContacts',ctrl.searchContacts); //Página donde podemos buscar nuevos contactos y enviar peticiones de contacto
-router.get('/chatroom', ctrl.chatroom); //Sala de chat
-router.get('/personalPage',ctrl.personalPage); //Página personal de cada usuario donde puede ver sus contactos y salas
+router.get('/about', ctrl.about); //Página de información
+router.get('/index', ctrl.index); //Página de login y registro
+router.get('/profile',middleware.checkToken , ctrl.profile); //Página en la que el usuario puede cambiar sus datos personales
+router.get('/searchContacts',middleware.checkToken ,ctrl.searchContacts); //Página donde podemos buscar nuevos contactos y enviar peticiones de contacto
+router.get('/chatroom',middleware.checkToken ,ctrl.chatroom); //Sala de chat
+router.get('/personalPage',middleware.checkToken ,ctrl.personalPage); //Página personal de cada usuario donde puede ver sus contactos y salas
 
 module.exports = router;
