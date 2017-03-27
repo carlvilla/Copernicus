289db7b52a4c76a38923f59c23aa30c0ca28e124
Carlos Villa <uo236851@uniovi.es>
2017-03-27 20:03:28 +0200

    Interfaz salas

---
+++ public/javascripts/navAnimation.js
@@ -0,0 +1,18 @@
+function openNav(nav) {
+    document.getElementById(nav).style.width = "180px";
+    document.getElementById("myAsistentesnav").style.marginLeft = "0px";
+    document.getElementById("Contactonav").style.width = "0px";
+}
+
+function resetNavContacto() {
+    document.getElementById("myAsistentesnav").style.marginLeft = "0px";
+}
+
+function closeNav(nav) {
+    document.getElementById(nav).style.width = "0";
+}
+
+function informacion() {
+    document.getElementById("Contactonav").style.width = "180px";
+
+}
