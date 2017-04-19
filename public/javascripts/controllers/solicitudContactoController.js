5b375cf1fd88c32a537e46b6b283731ee3789e56
Carlos Villa <uo236851@uniovi.es>
2017-04-19 14:43:13 +0200

    Autocompletar Añadir contacto y nueva opción Solicitudes de contacto

---
+++ public/javascripts/controllers/solicitudContactoController.js
@@ -0,0 +1,19 @@
+/**
+ * Created by carlosvillablanco on 19/04/2017.
+ */
+
+var webApp = angular.module('webApp');
+
+webApp.controller('solicitudContactoController', function ($scope) {
+
+    $scope.usernames = [
+        {username: "David10"},
+        {username: "Carlos"},
+        {username: "Carlos10"},
+        {username: "Carla"},
+        {username: "Carlos1111"},
+        {username: "David"},
+        {username: "David_username"},
+    ];
+
+});
