1babd1307403d02a013a3966567b28943937c31b
Carlos Villa <uo236851@uniovi.es>
2017-04-24 16:15:02 +0200

    Mostrar, mover y redimensionar módulos en las salas

--- public/javascripts/controllers/modulosController.js
+++ public/javascripts/controllers/modulosController.js
@@ -2,129 +2,72 @@ var webApp = angular.module('webApp');
 
 webApp.controller('modulosController', function ($scope, webSocketService) {
 
+    //Primero cargamos los htmls de los módulos
+    //Es necesario hacer esto en primer lugar, no se puede cargar el html una vez se va a añadir ya que no se cargará
+    //a tiempo
+    var htmlVideoChat;
+    var htmlPresentaciones;
+    var htmlDibujo;
 
-    $scope.activateView = function(ele) {
-        $compile(ele.contents())($scope);
-        $scope.$apply();
-    };
+    var numModulosMostrados = 0;
+
+    $.get("/modulos/chatVideo.ejs", function (html) {
+        htmlVideoChat = html;
+    });
+
+    $.get("/modulos/presentaciones.ejs", function (html) {
+        htmlPresentaciones = html;
+    });
+
+    $.get("/modulos/dibujo_corporativo.ejs", function (html) {
+        htmlDibujo = html;
+    });
 
 
-    $scope.addModule = function(modulo){
+    var options = {};
 
-        var myEl = angular.element( document.querySelector( '#modulos' ) );
+    var node = {
+        width: 5,
+        height: 5,
+        minWidth: 3,
+        maxWidth: 6,
+        minHeight: 3,
+        maxHeight: 5
+    }
 
-        switch(modulo)
-        {
+    $('.grid-stack').gridstack(options);
+
+    var grid = $('.grid-stack').data('gridstack');
+
+    $scope.addModule = function (modulo) {
+
+        //Por el momento solo se pueden utilizar 4 módulos simultáneamente
+        if(numModulosMostrados > 3){
+            return;
+        }
+
+        numModulosMostrados++;
+
+        switch (modulo) {
             case "chatVideo":
-                //Comunicar a todos los asistentes de que se añade este módulo
-                //Habría que comprobar que no haya más de un número X de módulos, por ejemplo que no se
-                //puedan utilizar más de 4 módulos de manera simultanea
-
-                console.log("Añadiendo chat de video");
-
-
-                $.get("/modulos/chatVideo.ejs", function (ejs) {
-                    myEl.html(ejs);
-                });
-
-                var mController = angular.element(document.getElementById("modulos"));
-                mController.scope().activateView(myEl);
-
-               // webSocketService.videoconferenciaManager.init();
-
-                //Una vez añadido, hay que desactivar la posibilidad de añadir el mismo servicio
-
-
+                moduloSeleccionado = htmlVideoChat;
                 break;
 
             case "presentaciones":
-                console.log("Añadiendo presentaciones");
-                $.get("/modulos/presentaciones.ejs", function (ejs) {
-                    myEl.append(ejs);
-                });
-
+                moduloSeleccionado = htmlPresentaciones;
                 break;
+
+            case "dibujo":
+                moduloSeleccionado = htmlDibujo;
 
             default:
                 break;
-
         }
 
+        grid.addWidget($(moduloSeleccionado),
+            0, 0, node.width, node.height, true, node.minWidth, node.maxWidth, node.minWidth, node.maxHeight);
+        return false;
 
-
-
-    }
-
-
-
-    /*
-    ko.components.register('dashboard-grid', {
-        viewModel: {
-            createViewModel: function (controller, componentInfo) {
-                var ViewModel = function (controller, componentInfo) {
-                    var grid = null;
-
-                    this.widgets = controller.widgets;
-
-                    this.afterAddWidget = function (items) {
-                        if (grid == null) {
-                            grid = $(componentInfo.element).find('.grid-stack').gridstack({
-                                auto: false
-                            }).data('gridstack');
-                        }
-
-                        var item = _.find(items, function (i) {
-                            return i.nodeType == 1
-                        });
-                        grid.addWidget(item);
-                        ko.utils.domNodeDisposal.addDisposeCallback(item, function () {
-                            grid.removeWidget(item);
-                        });
-                    };
-                };
-
-                return new ViewModel(controller, componentInfo);
-            }
-        },
-        template: {element:'modulo'}
-    });
-
-
-    $(function () {
-        var Controller = function (widgets) {
-            var self = this;
-
-            this.widgets = ko.observableArray(widgets);
-
-            this.addNewWidget = function () {
-                this.widgets.push({
-                    x: 0,
-                    y: 0,
-                    width: 6,
-                    height: 7,
-                    auto_position: true
-                });
-                return false;
-            };
-
-            this.deleteWidget = function (item) {
-                self.widgets.remove(item);
-                return false;
-            };
-        };
-
-        var widgets = [
-            {x: 7, y: 0, width: 6, height: 6},
-            {x: 0, y: 0, width: 6, height: 6},
-            {x: 7, y: 0, width: 6, height: 6},
-            {x: 0, y: 0, width: 6, height: 6}
-        ];
-
-        var controller = new Controller(widgets);
-        ko.applyBindings(controller);
-    });
-
-
-*/
+    };
 
 });
---
+++ public/javascripts/controllers/notificacionesController.js
@@ -0,0 +1,24 @@
+var webApp = angular.module('webApp');
+
+webApp.controller('notificacionesController', function ($scope, $http) {
+
+    $http({
+        method: "GET",
+        url: "api/solicitudesContacto"
+    }).then(successSolicitudes, error);
+
+    $http({
+        method: "GET",
+        url: "api/solicitudesContacto"
+    }).then(successSolicitudes, error);
+
+
+    function successSolicitudes(res) {
+        $scope.solicitudes = res.data;
+    }
+
+    function error(err){
+        console.log(err);
+    }
+
+});
