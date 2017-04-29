6e0692aaf0db025d23620df6cf896847669992a9
Carlos Villa <uo236851@uniovi.es>
2017-04-29 19:39:24 +0200

    Eliminar solicitudes aceptadas o ignoradas con jquery

--- public/javascripts/controllers/solicitudesContactoController.js
+++ public/javascripts/controllers/solicitudesContactoController.js
@@ -7,13 +7,39 @@ webApp.controller('solicitudesContactoController', function ($scope, $http) {
         url: "api/solicitudesContacto"
     }).then(successSolicitudes, error);
 
-
     function successSolicitudes(res) {
         $scope.solicitudes = res.data;
     }
 
     function error(err){
         //console.log(err);
+    }
+
+    $scope.aceptarSolicitud = function(usernameMandoSolicitud){
+
+        //Username de la persona que acepta la solicitud
+
+        $http({
+            method: "POST",
+            url: "api/aceptarSolicitud"
+        });
+
+        $("#solicitud-de-"+usernameMandoSolicitud).remove();
+
+    }
+
+    $scope.ignorarSolicitud = function(usernameMandoSolicitud){
+
+        //Username de la persona que acepta la solicituds
+
+        $http({
+            method: "POST",
+            url: "api/eliminarSolicitud"
+        });
+
+        $("#solicitud-de-"+usernameMandoSolicitud).remove();
+
+
     }
 
 
