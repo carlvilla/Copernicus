9d9edb7caf38243905505363770f91f8a8a23e2f
Carlos Villa <uo236851@uniovi.es>
2017-06-18 21:31:12 +0200

    Tests

--- test/test-unitarios.js
+++ test/test-unitarios.js
@@ -15,11 +15,11 @@ var request = require('supertest');
 var url = 'http://localhost:8080';
 
 var deleteDBQuery = "MATCH (n) DETACH DELETE n";
-var datosDBQuery;
 
 var tokenUsuario1;
 var tokenUsuario2;
-
+var tokenUsuario3;
+var tokenUsuario4;
 
 describe('Tests de usuarios', function () {
 
@@ -28,11 +28,6 @@ describe('Tests de usuarios', function () {
         db.query(deleteDBQuery, function (err, result) {
             if (err) throw err;
         });
-
-
-
-
-
     });
 
     /*
@@ -63,11 +58,11 @@ describe('Tests de usuarios', function () {
             it('Debería registrar correctamente a usuario1', function (done) {
 
                 var usuario = {
-                    'nombre': 'Usuario',
-                    'apellidos': 'ApellidosUsuario',
+                    'nombre': 'Usuario1',
+                    'apellidos': 'ApellidosUsuario1',
                     'username': 'usuario1',
-                    'email': 'usuario@email.com',
-                    'password': 'passwordtest'
+                    'email': 'usuario1@email.com',
+                    'password': 'usuario1'
                 }
 
                 request(url)
@@ -91,11 +86,11 @@ describe('Tests de usuarios', function () {
             it('Debería registrar correctamente a usuario2', function (done) {
 
                 var usuario = {
-                    'nombre': 'Usuario',
-                    'apellidos': 'ApellidosUsuario',
+                    'nombre': 'Usuario2',
+                    'apellidos': 'ApellidosUsuario2',
                     'username': 'usuario2',
-                    'email': 'usuario@email.com',
-                    'password': 'passwordtest'
+                    'email': 'usuario2@email.com',
+                    'password': 'usuario2'
                 }
 
                 request(url)
@@ -111,6 +106,65 @@ describe('Tests de usuarios', function () {
                         should.exist(res.body.token);//Comprobar que existe cookie de sesión
 
                         tokenUsuario2 = res.body.token;
+
+                        done();
+
+                    });
+            }),
+
+            it('Debería registrar correctamente a usuario3', function (done) {
+
+                var usuario = {
+                    'nombre': 'Usuario3',
+                    'apellidos': 'ApellidosUsuario3',
+                    'username': 'usuario3',
+                    'email': 'usuario3@email.com',
+                    'password': 'usuario3'
+                }
+
+                request(url)
+                    .post('/api/register')
+                    .send(usuario)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        res.status.should.be.equal(200);
+
+                        should.exist(res.body.token);//Comprobar que existe cookie de sesión
+
+                        tokenUsuario3 = res.body.token;
+
+                        done();
+
+                    });
+            }),
+
+
+            it('Debería registrar correctamente a usuario4', function (done) {
+
+                var usuario = {
+                    'nombre': 'Usuario4',
+                    'apellidos': 'ApellidosUsuario4',
+                    'username': 'usuario4',
+                    'email': 'usuario4@email.com',
+                    'password': 'usuario4'
+                }
+
+                request(url)
+                    .post('/api/register')
+                    .send(usuario)
+                    .end(function (err, res) {
+                        if (err) {
+                            throw err;
+                        }
+
+                        res.status.should.be.equal(200);
+
+                        should.exist(res.body.token);//Comprobar que existe cookie de sesión
+
+                        tokenUsuario4 = res.body.token;
 
                         done();
 
@@ -163,11 +217,11 @@ describe('Tests de usuarios', function () {
     });
 
     describe('Login de usuarios', function () {
-        it('Deberiamos logearnos con estos credenciales', function (done) {
+        it('Usuario1 deberia logearse con estos credenciales', function (done) {
 
             var credenciales = {
-                'username': 'usuario2',
-                'password': 'passwordtest'
+                'username': 'usuario1',
+                'password': 'usuario1'
             }
 
             request(url)
@@ -280,14 +334,14 @@ describe('Tests de usuarios', function () {
     });
 
     describe('Cambiar contraseña', function () {
-        it('Deberia cambiar la contraseña al usuario1', function (done) {
+        it('Deberia cambiar la contraseña al usuario4', function (done) {
             request(url)
                 .post('/api/modificarPass')
                 .set('Cookie', ['token = ' + tokenUsuario1])
                 .send({
-                    'username': 'usuario1',
-                    'password': 'passwordtest',
-                    'passwordNueva': 'passCambiada'
+                    'username': 'usuario4',
+                    'password': 'usuario4',
+                    'passwordNueva': 'passCambiada4'
                 })
                 .end(function (err, res) {
                     if (err) {
@@ -323,7 +377,118 @@ describe('Tests de usuarios', function () {
 
 
     describe('Crear sala', function () {
-        it('Deberia crear una sala cuyo administrador es el usuario1 y donde el usuario2 es moderador', function (done) {
+        it("Deberia crear una sala llamada 'Sala de pruebas' cuyo administrador es el usuario1 y se le envía una solicitud de unión como moderador" +
+            " al usuario2 y como miembro al usuario3", function (done) {
+            request(url)
+                .post('/api/createSala')
+                .set('Cookie', ['token = ' + tokenUsuario1])
+                .send({
+                    'sala': {
+                        nombre: 'Sala de pruebas',
+                        descripcion: 'Sala creada mediantes pruebas automatizadas',
+                        idSala: 1,
+                        fotoPorDefecto: true
+                    },
+                    'usuarios': [{'username': 'usuario2', 'permisos': 'Moderador'},
+                        {'username': 'usuario2', 'permisos': 'Miembro'}]
+
+                }).end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+
+                res.status.should.be.equal(204);
+                done();
+
+            })
+        })
+
+
+        it("Deberia crear una sala llamada 'Sala de pruebas 2' cuyo administrador es el usuario2 y se le envía una solicitud de unión como moderador" +
+            " al usuario1", function (done) {
+            request(url)
+                .post('/api/createSala')
+                .set('Cookie', ['token = ' + tokenUsuario1])
+                .send({
+                    'sala': {
+                        nombre: 'Sala de pruebas 2',
+                        descripcion: 'Esta es la descripción de la sala de pruebas 3',
+                        idSala: 1,
+                        fotoPorDefecto: true
+                    },
+                    'usuarios': [{'username': 'usuario1', 'permisos': 'Moderador'}]
+
+                }).end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+
+                res.status.should.be.equal(204);
+                done();
+
+            })
+        })
+
+        it("Deberia crear una sala llamada 'Sala de pruebas 3' cuyo administrador es el usuario2 y se le envía una solicitud de unión como miembro" +
+            " al usuario1", function (done) {
+            request(url)
+                .post('/api/createSala')
+                .set('Cookie', ['token = ' + tokenUsuario1])
+                .send({
+                    'sala': {
+                        nombre: 'Sala de pruebas 3',
+                        descripcion: 'Sala para realizar pruebas',
+                        idSala: 1,
+                        fotoPorDefecto: true
+                    },
+                    'usuarios': [{'username': 'usuario1', 'permisos': 'Miembro'}]
+
+                }).end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+
+                res.status.should.be.equal(204);
+                done();
+
+            })
+        })
+    });
+
+    describe('Aceptar solicitudes de unión a salas', function () {
+        it('El usuario1 deberia aceptar la solicitud de unión a la sala una sala cuyo administrador es el usuario1 y se le envía una solicitud de unión como moderador' +
+            ' al usuario2 y como miembro al usuario3', function (done) {
+            request(url)
+                .post('/api/createSala')
+                .set('Cookie', ['token = ' + tokenUsuario1])
+                .send({
+                    'sala': {
+                        nombre: 'Sala de pruebas',
+                        descripcion: 'Sala creada mediantes pruebas automatizadas',
+                        idSala: 1,
+                        fotoPorDefecto: true
+                    },
+                    'usuarios': [{'username': 'usuario2', 'permisos': 'Moderador'},
+                        {'username': 'usuario2', 'permisos': 'Miembro'}]
+
+                }).end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+
+                res.status.should.be.equal(204);
+                done();
+
+            })
+        })
+
+
+        it('Deberia crear una sala cuyo administrador es el usuario2 y se le envía una solicitud de unión como moderador' +
+            ' al usuario1', function (done) {
             request(url)
                 .post('/api/createSala')
                 .set('Cookie', ['token = ' + tokenUsuario1])
@@ -348,7 +513,35 @@ describe('Tests de usuarios', function () {
             })
         })
 
+        it('Deberia crear una sala cuyo administrador es el usuario2 y se le envía una solicitud de unión como miembro' +
+            ' al usuario1', function (done) {
+            request(url)
+                .post('/api/createSala')
+                .set('Cookie', ['token = ' + tokenUsuario1])
+                .send({
+                    'sala': {
+                        nombre: 'Sala de pruebas',
+                        descripcion: 'Sala creada mediantes pruebas automatizadas',
+                        idSala: 1,
+                        fotoPorDefecto: true
+                    },
+                    'usuarios': [{'username': 'usuario2', 'permisos': 'Moderador'}]
+
+                }).end(function (err, res) {
+
+                if (err) {
+                    throw err;
+                }
+
+                res.status.should.be.equal(204);
+                done();
+
+            })
+        })
     });
+
+
+
 
 
 });
